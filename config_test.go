d347159e019b4b5bfe114c2c6b4de43cf36e215e
Seth Vargo <sethvargo@gmail.com>
2014-10-10 16:39:54 -0400

    Use reflect.DeepEqual to test config values
    Doing so will ensure we have the exact configuration object, not just
    bits and pieces

--- config_test.go
+++ config_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -57,27 +58,14 @@ func TestParseConfig_mapstructureError(t *testing.T) {
 	}
 }
 
-// Test that the config reads the "consul" key
-func TestParseConfig_readsConsulKey(t *testing.T) {
+// Test that the config is parsed correctly
+func TestParseConfig_correctValues(t *testing.T) {
 	configFile := createTempfile([]byte(`
     consul = "nyc1.demo.consul.io"
-  `), t)
-	defer deleteTempfile(configFile, t)
+    token = "abcd1234"
+    wait = "5s:10s"
+    once = true
 
-	config, err := ParseConfig(configFile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "nyc1.demo.consul.io"
-	if config.Consul != expected {
-		t.Fatalf("expected config.Consul to be %q, was %q", expected, config.Consul)
-	}
-}
-
-// Test that the config reads the "template" keys
-func TestParseConfig_readsTemplateKeys(t *testing.T) {
-	configFile := createTempfile([]byte(`
     template {
       source = "nginx.conf.ctmpl"
       destination  = "/etc/nginx/nginx.conf"
@@ -86,7 +74,7 @@ func TestParseConfig_readsTemplateKeys(t *testing.T) {
     template {
       source = "redis.conf.ctmpl"
       destination  = "/etc/redis/redis.conf"
-      command = "service restart redis"
+      command = "service redis restart"
     }
   `), t)
 	defer deleteTempfile(configFile, t)
@@ -96,98 +84,30 @@ func TestParseConfig_readsTemplateKeys(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if len(config.ConfigTemplates) != 2 {
-		t.Fatalf("expected 3 ConfigTemplates, but slice had %d", len(config.ConfigTemplates))
+	expected := &Config{
+		Path:   configFile.Name(),
+		Consul: "nyc1.demo.consul.io",
+		Token:  "abcd1234",
+		Wait: &Wait{
+			Min: time.Second * 5,
+			Max: time.Second * 10,
+		},
+		WaitRaw: "5s:10s",
+		Once:    true,
+		ConfigTemplates: []*ConfigTemplate{
+			&ConfigTemplate{
+				Source:      "nginx.conf.ctmpl",
+				Destination: "/etc/nginx/nginx.conf",
+			},
+			&ConfigTemplate{
+				Source:      "redis.conf.ctmpl",
+				Destination: "/etc/redis/redis.conf",
+				Command:     "service redis restart",
+			},
+		},
 	}
-
-	nginx := config.ConfigTemplates[0]
-	expected := "nginx.conf.ctmpl"
-	if nginx.Source != expected {
-		t.Errorf("expected nginx.Source to be %q, was %q", expected, nginx.Source)
-	}
-	expected = "/etc/nginx/nginx.conf"
-	if nginx.Destination != expected {
-		t.Errorf("expected nginx.Destination to be %q, was %q", expected, nginx.Destination)
-	}
-	expected = ""
-	if nginx.Command != expected {
-		t.Errorf("expected nginx.Command to be %q, was %q", expected, nginx.Command)
-	}
-
-	redis := config.ConfigTemplates[1]
-	expected = "redis.conf.ctmpl"
-	if redis.Source != expected {
-		t.Errorf("expected redis.Source to be %q, was %q", expected, redis.Source)
-	}
-	expected = "/etc/redis/redis.conf"
-	if redis.Destination != expected {
-		t.Errorf("expected redis.Destination to be %q, was %q", expected, redis.Destination)
-	}
-	expected = "service restart redis"
-	if redis.Command != expected {
-		t.Errorf("expected redis.Command to be %q, was %q", expected, redis.Command)
-	}
-}
-
-// Test that the config reads the "token" key
-func TestParseConfig_readsTokenKey(t *testing.T) {
-	configFile := createTempfile([]byte(`
-    token = "abcd1234"
-  `), t)
-	defer deleteTempfile(configFile, t)
-
-	config, err := ParseConfig(configFile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "abcd1234"
-	if config.Token != expected {
-		t.Fatalf("expected config.Token to be %q, was %q", expected, config.Token)
-	}
-}
-
-// Test that mapstructure does not propagate the "path" key from the config
-// and properly sets it to the given path argument.
-func TestParseConfig_setsPath(t *testing.T) {
-	configFile := createTempfile([]byte(`
-    path = "/path/to/config.json"
-  `), t)
-	defer deleteTempfile(configFile, t)
-
-	config, err := ParseConfig(configFile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := configFile.Name()
-	if config.Path != expected {
-		t.Fatalf("expected config.Path to be %q, was %q", expected, config.Token)
-	}
-}
-
-// Test that the config reads the "wait" key
-func TestParseConfig_readsWaitKey(t *testing.T) {
-	configFile := createTempfile([]byte(`
-    wait = "5s:10s"
-  `), t)
-	defer deleteTempfile(configFile, t)
-
-	config, err := ParseConfig(configFile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	wait := config.Wait
-
-	expectedMin := time.Duration(5) * time.Second
-	if wait.Min != expectedMin {
-		t.Errorf("expected %q to equal %q", wait.Min, expectedMin)
-	}
-
-	expectedMax := time.Duration(10) * time.Second
-	if wait.Max != expectedMax {
-		t.Errorf("expected %q to equal %q", wait.Max, expectedMin)
+	if !reflect.DeepEqual(config, expected) {
+		t.Fatalf("expected \n%#v\n\n, got \n\n%#v", expected, config)
 	}
 }
 
@@ -206,23 +126,5 @@ func TestParseConfig_parseWaitError(t *testing.T) {
 	expectedErr := "invalid duration not_valid"
 	if !strings.Contains(err.Error(), expectedErr) {
 		t.Fatalf("expected error %q to contain %q", err.Error(), expectedErr)
-	}
-}
-
-// Test that the config reads the "once" key
-func TestParseConfig_readsOnceKey(t *testing.T) {
-	configFile := createTempfile([]byte(`
-    once = true
-  `), t)
-	defer deleteTempfile(configFile, t)
-
-	config, err := ParseConfig(configFile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := true
-	if config.Once != expected {
-		t.Fatalf("expected config.Once to be %q, was %q", expected, config.Once)
 	}
 }
