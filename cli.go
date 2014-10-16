ee7a2bc93ddc276f046749b2021d955fe7c6411e
Seth Vargo <sethvargo@gmail.com>
2014-10-15 22:04:21 -0400

    Remove CLI extra Parse method, make "dry" and "once" flags

--- cli.go
+++ cli.go
@@ -39,10 +39,65 @@ type CLI struct {
 // Run accepts a slice of arguments and returns an int representing the exit
 // status from the command.
 func (cli *CLI) Run(args []string) int {
-	config, status, err := cli.Parse(args)
-	if err != nil {
-		fmt.Fprint(cli.errStream, err.Error())
-		return status
+	var version, dry, once bool
+	var config = new(Config)
+
+	// Parse the flags and options
+	cmd := filepath.Base(args[0])
+	flags := flag.NewFlagSet("consul-template", flag.ContinueOnError)
+	flags.Usage = func() { fmt.Fprint(cli.outStream, usage) }
+	flags.SetOutput(cli.outStream)
+	flags.StringVar(&config.Consul, "consul", "",
+		"address of the Consul instance")
+	flags.Var((*configTemplateVar)(&config.ConfigTemplates), "template",
+		"new template declaration")
+	flags.StringVar(&config.Token, "token", "",
+		"a consul API token")
+	flags.StringVar(&config.WaitRaw, "wait", "",
+		"the minimum(:maximum) to wait before rendering a new template")
+	flags.StringVar(&config.Path, "config", "",
+		"the path to a config file on disk")
+	flags.BoolVar(&once, "once", false,
+		"do not run as a daemon")
+	flags.BoolVar(&dry, "dry", false,
+		"write generated templates to stdout")
+	flags.BoolVar(&version, "version", false, "display the version")
+
+	// If there was a parser error, stop
+	if err := flags.Parse(args[1:]); err != nil {
+		fmt.Fprintf(cli.errStream, "%s\n\n%s", err.Error(), usage)
+		return ExitCodeParseFlagsError
+	}
+
+	// If the version was requested, return an "error" containing the version
+	// information. This might sound weird, but most *nix applications actually
+	// print their version on stderr anyway.
+	if version {
+		fmt.Fprintf(cli.errStream, "%s v%s\n", cmd, Version)
+		return ExitCodeOK
+	}
+
+	// Parse the raw wait value into a Wait object
+	if config.WaitRaw != "" {
+		wait, err := ParseWait(config.WaitRaw)
+		if err != nil {
+			fmt.Fprintf(cli.errStream, "%s\n\n%s", err.Error(), usage)
+			return ExitCodeParseWaitError
+		}
+		config.Wait = wait
+	}
+
+	// Merge a path config with the command line options. Command line options
+	// take precedence over config file options for easy overriding.
+	if config.Path != "" {
+		fileConfig, err := ParseConfig(config.Path)
+		if err != nil {
+			fmt.Fprintf(cli.errStream, "%s\n\n%s", err.Error(), usage)
+			return ExitCodeParseConfigError
+		}
+
+		fileConfig.Merge(config)
+		config = fileConfig
 	}
 
 	mapper, err := NewMapper(config.ConfigTemplates)
@@ -89,68 +144,6 @@ func (cli *CLI) Run(args []string) int {
 	}
 
 	return ExitCodeOK
-}
-
-// Parse accepts a slice of command line flags and returns a generated Config
-// object, an exit status, and any errors that occurred when parsing the flags.
-func (cli *CLI) Parse(args []string) (*Config, int, error) {
-	var version = false
-	var config = new(Config)
-
-	cmd := filepath.Base(args[0])
-
-	flags := flag.NewFlagSet("consul-template", flag.ContinueOnError)
-	flags.Usage = func() { fmt.Fprint(cli.outStream, usage) }
-	flags.SetOutput(cli.outStream)
-	flags.StringVar(&config.Consul, "consul", "",
-		"address of the Consul instance")
-	flags.Var((*configTemplateVar)(&config.ConfigTemplates), "template",
-		"new template declaration")
-	flags.StringVar(&config.Token, "token", "",
-		"a consul API token")
-	flags.StringVar(&config.WaitRaw, "wait", "",
-		"the minimum(:maximum) to wait before rendering a new template")
-	flags.StringVar(&config.Path, "config", "",
-		"the path to a config file on disk")
-	flags.BoolVar(&config.Once, "once", false,
-		"do not run as a daemon")
-	flags.BoolVar(&config.Dry, "dry", false,
-		"write generated templates to stdout")
-	flags.BoolVar(&version, "version", false, "display the version")
-
-	if err := flags.Parse(args[1:]); err != nil {
-		return nil, ExitCodeParseFlagsError, fmt.Errorf("%s\n\n%s", err, usage)
-	}
-
-	// If the version was requested, return an "error" containing the version
-	// information. This might sound weird, but most *nix applications actually
-	// print their version on stderr anyway.
-	if version {
-		return nil, ExitCodeOK, fmt.Errorf("%s v%s\n", cmd, Version)
-	}
-
-	// Parse the raw wait value into a Wait object
-	if config.WaitRaw != "" {
-		wait, err := ParseWait(config.WaitRaw)
-		if err != nil {
-			return nil, ExitCodeParseWaitError, fmt.Errorf("%s\n\n%s", err, usage)
-		}
-		config.Wait = wait
-	}
-
-	// Merge a path config with the command line options. Command line options
-	// take precedence over config file options for easy overriding.
-	if config.Path != "" {
-		fileConfig, err := ParseConfig(config.Path)
-		if err != nil {
-			return nil, ExitCodeParseConfigError, fmt.Errorf("%s\n\n%s", err, usage)
-		}
-
-		fileConfig.Merge(config)
-		config = fileConfig
-	}
-
-	return config, ExitCodeOK, nil
 }
 
 const usage = `
