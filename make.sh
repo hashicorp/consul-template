0e1e3ca8aa366cea7bdee16ac80e6061f8a86577
Andrew Watson <andrew@ionicsecurity.com>
2014-12-01 16:45:08 -0500

    cleanup

--- make.sh
+++ make.sh
@@ -6,7 +6,6 @@ case $1 in
 
 runbuild)
         echo "BUILDING consul-template"
-        # CGO_ENABLED=0 godep go build -a -ldflags '-s' -o run/enterpriseManager
         go get -d ./...
         go build -ldflags '-linkmode external -extldflags "-static" ' -o bin/$NAME
 ;;
@@ -15,8 +14,6 @@ runbuild)
         echo "RUNNING BUILD IN DOCKER CONTAINER"
         docker run --rm -it -v "$(pwd)":/usr/src/myapp -w /usr/src/myapp golang:1.3.3 ./make.sh runbuild
 
-        # cd run
-        # docker build --tag ionic/consultemplate .
 ;;
 
 esac
