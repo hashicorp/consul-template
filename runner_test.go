a10b8d69caaccdaff2880960fac5422ae12398aa
Seth Vargo <sethvargo@gmail.com>
2015-01-16 17:38:28 -0500

    Add a "test"

--- runner_test.go
+++ runner_test.go
@@ -9,9 +9,11 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 
 	dep "github.com/hashicorp/consul-template/dependency"
 	"github.com/hashicorp/consul-template/test"
+	"github.com/hashicorp/consul-template/watch"
 )
 
 func TestNewRunner_initialize(t *testing.T) {
@@ -476,6 +478,58 @@ func TestRun_multipleTemplatesRunsCommands(t *testing.T) {
 
 	if _, err := os.Stat(touch2.Name()); err == nil {
 		t.Errorf("expected second command to not run, but touch exists")
+	}
+}
+
+// Warning: this is a super fragile and time-dependent test. If it's failing,
+// check the demo Consul cluster and your own sanity before you assume your
+// code broke something...
+func TestRunner_quiescence(t *testing.T) {
+	in := test.CreateTempfile([]byte(`
+    {{ range service "consul" "any" }}{{.Node}}{{ end }}
+  `), t)
+	defer test.DeleteTempfile(in, t)
+
+	out := test.CreateTempfile(nil, t)
+	test.DeleteTempfile(out, t)
+
+	config := &Config{
+		Consul: "demo.consul.io",
+		Wait: &watch.Wait{
+			Min: 500 * time.Millisecond,
+			Max: 1 * time.Second,
+		},
+		ConfigTemplates: []*ConfigTemplate{
+			&ConfigTemplate{
+				Source:      in.Name(),
+				Destination: out.Name(),
+			},
+		},
+	}
+
+	runner, err := NewRunner(config, false, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go runner.Start()
+	defer runner.Stop()
+
+	min := time.After(400 * time.Millisecond)
+	max := time.After(1 * time.Second)
+	for {
+		select {
+		case <-min:
+			if _, err = os.Stat(out.Name()); !os.IsNotExist(err) {
+				t.Errorf("expected quiescence timer to not fire for yet")
+			}
+			continue
+		case <-max:
+			if _, err = os.Stat(out.Name()); os.IsNotExist(err) {
+				t.Errorf("expected template to be rendered by now")
+			}
+			return
+		}
 	}
 }
 
