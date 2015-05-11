b93cc49d77d309110342edb9f0451bc318dc73ff
Seth Vargo <sethvargo@gmail.com>
2015-05-11 10:17:00 -0400

    Add docs for Bool

--- bool.go
+++ bool.go
@@ -1,8 +1,10 @@
 package main
 
+// Bool is a special bool type that provides the ability to be "unset".
 type Bool int16
 
 const (
+	// BoolTrue, BoolUnset, and BoolFalse are true, "nil", and false respectively.
 	BoolTrue  Bool = 1
 	BoolUnset Bool = 0
 	BoolFalse Bool = -1
