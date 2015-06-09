14cf110504f3089a3c96a36126bd6d5fe39df37a
Seth Vargo <sethvargo@gmail.com>
2015-06-09 13:27:43 -0400

    Release 0.10.0

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 const Name = "consul-template"
 
 // Version is the current version of this application.
-const Version = "0.9.0.dev"
+const Version = "0.10.0"
 
 func main() {
 	cli := NewCLI(os.Stdout, os.Stderr)
