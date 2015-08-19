161fce6ccc101f600b7d2eb7a6c2ad7fbbb11a68
Seth Vargo <sethvargo@gmail.com>
2015-08-18 21:05:57 -0400

    Pass the config to the watcher

--- runner.go
+++ runner.go
@@ -975,6 +975,7 @@ func newWatcher(config *Config, clients *dep.ClientSet, once bool) (*watch.Watch
 		RetryFunc: func(current time.Duration) time.Duration {
 			return config.Retry
 		},
+		RenewVault: config.Vault.Renew,
 	})
 	if err != nil {
 		return nil, err
