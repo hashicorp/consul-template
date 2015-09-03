f9c5810fbebb4061b6db428322f741100e8ee796
Seth Vargo <sethvargo@gmail.com>
2015-09-03 11:13:44 -0400

    Fix failing Vault test

--- dependency/vault_token_test.go
+++ dependency/vault_token_test.go
@@ -10,23 +10,15 @@ func TestVaultTokenFetch(t *testing.T) {
 	clients, server := testVaultServer(t)
 	defer server.Stop()
 
-	vault, err := clients.Vault()
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	// Create a new token - the default token is a root token and is therefore
 	// not renewable
-	token, err := vault.Auth().Token().Create(&api.TokenCreateRequest{
+	secret, err := clients.vault.Auth().Token().Create(&api.TokenCreateRequest{
 		Lease: "1h",
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// Set the new token on the client so we can try to renew
-	newToken := token.Auth.ClientToken
-	vault.SetToken(newToken)
+	clients.vault.SetToken(secret.Auth.ClientToken)
 
 	dep := new(VaultToken)
 	results, _, err := dep.Fetch(clients, nil)
