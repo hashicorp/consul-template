2431448c7edd50fd2a2165f4a709f0bb1736e0a3
Seth Vargo <sethvargo@gmail.com>
2016-03-31 17:32:17 +0200

    Load VAULT_TOKEN from the environment into config
    This fixes an issue where Vault tokens were not renewed unless they were
    hard-coded in the configuration. This is obviously a non-desirable use
    case, since environment variables are preferred. However, it was
    impossible to enable renewal on the token if specified via the
    environment variable prior to this commit.

--- config.go
+++ config.go
@@ -557,6 +557,10 @@ func DefaultConfig() *Config {
 		config.Vault.Address = v
 	}
 
+	if v := os.Getenv("VAULT_TOKEN"); v != "" {
+		config.Vault.Token = v
+	}
+
 	if v := os.Getenv("VAULT_CAPATH"); v != "" {
 		config.Vault.SSL.Cert = v
 	}
