2eb07246eeab99536a383be9ddb4e2718f74c31b
Seth Vargo <sethvargo@gmail.com>
2016-09-22 15:38:23 -0400

    Prevent infinite renewal loop to Vault

--- dependency/vault_token.go
+++ dependency/vault_token.go
@@ -37,6 +37,13 @@ func (d *VaultToken) Fetch(clients *ClientSet, opts *QueryOptions) (interface{},
 	// try to renew.
 	if opts.WaitIndex != 0 && d.leaseDuration != 0 {
 		duration := time.Duration(d.leaseDuration/2.0) * time.Second
+
+		if duration < 1*time.Second {
+			log.Printf("[DEBUG] (%s) increasing sleep to 1s (was %q)",
+				d.Display(), duration)
+			duration = 1 * time.Second
+		}
+
 		log.Printf("[DEBUG] (%s) sleeping for %q", d.Display(), duration)
 		select {
 		case <-d.stopCh:
@@ -64,15 +71,15 @@ func (d *VaultToken) Fetch(clients *ClientSet, opts *QueryOptions) (interface{},
 		Data:          token.Data,
 	}
 
-	leaseDuration := secret.LeaseDuration
+	leaseDuration := token.Auth.LeaseDuration
 	if leaseDuration == 0 {
-		log.Printf("[WARN] (%s) lease duration is 0, setting to 5m", d.Display())
-		leaseDuration = 5 * 60
+		log.Printf("[WARN] (%s) lease duration is 0, setting to 5s", d.Display())
+		leaseDuration = 5
 	}
 
 	d.Lock()
 	d.leaseID = secret.LeaseID
-	d.leaseDuration = secret.LeaseDuration
+	d.leaseDuration = leaseDuration
 	d.Unlock()
 
 	log.Printf("[DEBUG] (%s) successfully renewed token", d.Display())
