613d77aa030127235c3066bfe98a0a3ec9ef5925
Jeremy Yoder <jyoder@magnetic.com>
2017-02-13 14:30:01 -0500

    Remove float types from modulo

--- template/funcs.go
+++ template/funcs.go
@@ -1107,8 +1107,6 @@ func modulo(b, a interface{}) (interface{}, error) {
 			return av.Int() % bv.Int(), nil
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 			return av.Int() % int64(bv.Uint()), nil
-		case reflect.Float32, reflect.Float64:
-			return float64(av.Int()) % bv.Float(), nil
 		default:
 			return nil, fmt.Errorf("modulo: unknown type for %q (%T)", bv, b)
 		}
@@ -1118,19 +1116,6 @@ func modulo(b, a interface{}) (interface{}, error) {
 			return int64(av.Uint()) % bv.Int(), nil
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 			return av.Uint() % bv.Uint(), nil
-		case reflect.Float32, reflect.Float64:
-			return float64(av.Uint()) % bv.Float(), nil
-		default:
-			return nil, fmt.Errorf("modulo: unknown type for %q (%T)", bv, b)
-		}
-	case reflect.Float32, reflect.Float64:
-		switch bv.Kind() {
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			return av.Float() % float64(bv.Int()), nil
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			return av.Float() % float64(bv.Uint()), nil
-		case reflect.Float32, reflect.Float64:
-			return av.Float() % bv.Float(), nil
 		default:
 			return nil, fmt.Errorf("modulo: unknown type for %q (%T)", bv, b)
 		}
