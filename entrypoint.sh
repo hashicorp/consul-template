4778e6f43be885dbd56c01b80da8ad34d50d883d
Yash Mehrotra <yashmehrotra95@gmail.com>
2019-06-25 14:46:12 +0530

    Exit if VAULT_TOKEN not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,10 @@ export VAULT_SKIP_VERIFY=true
 export CONSUL_HTTP_ADDR=$CONSUL_ADDR:8500
 export SERVICE_ACCOUNT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export VAULT_TOKEN=$(vault write auth/kubernetes/login role=read jwt=$SERVICE_ACCOUNT_TOKEN | grep -m 1 token | awk '{print $2}')
+
+if [ -z "$VAULT_TOKEN" ]; then
+    echo "VAULT_TOKEN not set. Exiting ..."
+    exit 1
+fi
+
 exec /usr/bin/consul-template "$@"
