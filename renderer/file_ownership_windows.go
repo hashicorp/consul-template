5be1f708cd67dc7136e8534b924cb67cd244cd6f
Lukas W <lukaswhl@gmail.com>
2022-02-04 09:33:22 +0100

    fail when trying to chown on Windows

--- renderer/file_ownership_windows.go
+++ renderer/file_ownership_windows.go
@@ -4,27 +4,35 @@
 package renderer
 
 import (
-	"log"
+	"fmt"
 )
 
+var notSupportedError error = fmt.Errorf("managing file ownership is not supported on Windows")
+
 func setFileOwnership(path string, uid, gid int) error {
-	return nil
+	if uid == -1 && gid == -1 {
+		return nil
+	}
+	return notSupportedError
 }
 
 func isChownNeeded(path string, wantedUid, wantedGid int) (bool, error) {
-	return false, nil
+	if wantedUid == -1 && wantedGid == -1 {
+		return false, nil
+	}
+	return false, notSupportedError
 }
 
 func lookupUser(user string) (int, error) {
-	if user != "" {
-		log.Printf("[WARN] (runner) cannot set user for rendered files on Windows")
+	if user == "" {
+		return -1, nil
 	}
-	return -1, nil
+	return 0, notSupportedError
 }
 
 func lookupGroup(group string) (int, error) {
-	if group != "" {
-		log.Printf("[WARN] (runner) cannot set group for rendered files on Windows")
+	if group == "" {
+		return -1, nil
 	}
-	return -1, nil
+	return 0, notSupportedError
 }
