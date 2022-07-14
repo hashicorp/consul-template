f7c638501fc07899797bcf6a58e5c55e8533d3ce
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-07-14 15:23:57 -0400

    Nomad SV: Add template functions
    - new Nomad template functions
    - add to funcmap
    - add tests

--- template/nomad_funcs.go
+++ template/nomad_funcs.go
@@ -75,3 +75,109 @@ func nomadServiceFunc(b *Brain, used, missing *dep.Set) func(...interface{}) ([]
 		return nil, nil
 	}
 }
+
+// nomadSecureVariableItemsFunc returns a given secure variable rooted at the
+// items map.
+func nomadSecureVariableItemsFunc(b *Brain, used, missing *dep.Set) func(string) (dep.NomadSVItems, error) {
+	return func(s string) (dep.NomadSVItems, error) {
+		if len(s) == 0 {
+			return nil, nil
+		}
+
+		d, err := dep.NewSVGetQuery(s)
+		if err != nil {
+			return nil, err
+		}
+		d.EnableBlocking()
+
+		used.Add(d)
+
+		if value, ok := b.Recall(d); ok {
+			if value == nil {
+				return nil, nil
+			}
+			return value.(dep.NomadSVItems), nil
+		}
+
+		missing.Add(d)
+
+		return nil, nil
+	}
+}
+
+// nomadSecureVariableExistsFunc returns true if a secure variable exists, false
+// otherwise.
+func nomadSecureVariableExistsFunc(b *Brain, used, missing *dep.Set) func(string) (bool, error) {
+	return func(s string) (bool, error) {
+		if len(s) == 0 {
+			return false, nil
+		}
+
+		d, err := dep.NewSVGetQuery(s)
+		if err != nil {
+			return false, err
+		}
+
+		used.Add(d)
+
+		if value, ok := b.Recall(d); ok {
+			return value != nil, nil
+		}
+
+		missing.Add(d)
+
+		return false, nil
+	}
+}
+
+func nomadSafeSecureVariablesFunc(b *Brain, used, missing *dep.Set) func(string) ([]*dep.NomadSVMeta, error) {
+	// call nomadSecureVariablesFunc but explicitly mark that empty data set
+	// returned on monitored secure variable prefix is NOT safe
+	return nomadSecureVariablesFunc(b, used, missing, false)
+}
+
+// nomadSecureVariablesFunc returns or accumulates nomad secure variable prefix
+// list dependencies.
+func nomadSecureVariablesFunc(b *Brain, used, missing *dep.Set, emptyIsSafe bool) func(string) ([]*dep.NomadSVMeta, error) {
+	return func(s string) ([]*dep.NomadSVMeta, error) {
+		result := []*dep.NomadSVMeta{}
+
+		if len(s) == 0 {
+			return result, nil
+		}
+
+		d, err := dep.NewSVListQuery(s)
+		if err != nil {
+			return result, err
+		}
+
+		used.Add(d)
+
+		// Only return non-empty top-level keys
+		value, ok := b.Recall(d)
+		if ok {
+			result = append(result, value.([]*dep.NomadSVMeta)...)
+
+			if len(result) == 0 {
+				if emptyIsSafe {
+					// Operator used potentially unsafe nomadSecureVariables
+					// function in the template instead of the safe version.
+					return result, nil
+				}
+			} else {
+				// non empty result is good so we just return the data
+				return result, nil
+			}
+
+			// If we reach this part of the code result is completely empty as
+			// value returned no secure variables. Operator selected to use
+			// safeSecureVariables on the specific secure variable prefix so we
+			// will refuse to render template by marking d as missing
+		}
+
+		// b.Recall either returned an error or safeSecureVariables entered unsafe case
+		missing.Add(d)
+
+		return result, nil
+	}
+}
