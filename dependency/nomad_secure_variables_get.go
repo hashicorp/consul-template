b1a4c3e34d0c0f5cdf85eec7da3eed18e7139e2d
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-07-14 15:18:05 -0400

    Nomad SV: Secure Variables Get dependency

---
+++ dependency/nomad_secure_variables_get.go
@@ -0,0 +1,106 @@
+package dependency
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	// Ensure implements
+	_ Dependency = (*SVGetQuery)(nil)
+
+	// SVGetQueryRe is the regular expression to use.
+	SVGetQueryRe = regexp.MustCompile(`\A` + svPathRe + `\z`)
+)
+
+// SVGetQuery queries the KV store for a single key.
+type SVGetQuery struct {
+	stopCh chan struct{}
+
+	path       string
+	blockOnNil bool
+}
+
+// NewSVGetQuery parses a string into a dependency.
+func NewSVGetQuery(s string) (*SVGetQuery, error) {
+	if s != "" && !SVGetQueryRe.MatchString(s) {
+		return nil, fmt.Errorf("nomad.secure_variables.get: invalid format: %q", s)
+	}
+
+	m := regexpMatch(SVGetQueryRe, s)
+	return &SVGetQuery{
+		stopCh: make(chan struct{}, 1),
+		path:   m["path"],
+	}, nil
+}
+
+// Fetch queries the Nomad API defined by the given client.
+func (d *SVGetQuery) Fetch(clients *ClientSet, opts *QueryOptions) (interface{}, *ResponseMetadata, error) {
+	select {
+	case <-d.stopCh:
+		return nil, nil, ErrStopped
+	default:
+	}
+
+	opts = opts.Merge(&QueryOptions{})
+
+	log.Printf("[TRACE] %s: GET %s", d, &url.URL{
+		Path:     "/v1/var/" + d.path,
+		RawQuery: opts.String(),
+	})
+
+	// NOTE: The Peek method of the Nomad SV API will check a value, return it
+	// if it exists, but return a nil value and NO error if it is not found.
+	sv, qm, err := clients.Nomad().SecureVariables().Peek(d.path, opts.ToNomadOpts())
+	if err != nil {
+		return nil, nil, errors.Wrap(err, d.String())
+	}
+
+	rm := &ResponseMetadata{
+		LastIndex:   qm.LastIndex,
+		LastContact: qm.LastContact,
+		BlockOnNil:  d.blockOnNil,
+	}
+
+	if sv == nil {
+		log.Printf("[TRACE] %s: returned nil", d)
+		return nil, rm, nil
+	}
+
+	items := NewNomadSecureVariable(sv).Items
+	log.Printf("[TRACE] %s: returned %q", d, sv.Path)
+	return items, rm, nil
+}
+
+// EnableBlocking turns this into a blocking KV query.
+func (d *SVGetQuery) EnableBlocking() {
+	d.blockOnNil = true
+}
+
+// CanShare returns a boolean if this dependency is shareable.
+func (d *SVGetQuery) CanShare() bool {
+	return true
+}
+
+// String returns the human-friendly version of this dependency.
+func (d *SVGetQuery) String() string {
+	path := d.path
+	if d.blockOnNil {
+		return fmt.Sprintf("nomad.secure_variables.block(%s)", path)
+	}
+	return fmt.Sprintf("nomad.secure_variables.get(%s)", path)
+}
+
+// Stop halts the dependency's fetch function.
+func (d *SVGetQuery) Stop() {
+	close(d.stopCh)
+}
+
+// Type returns the type of this dependency.
+func (d *SVGetQuery) Type() Type {
+	return TypeNomad
+}
---
+++ dependency/nomad_secure_variables_get_test.go
@@ -0,0 +1,294 @@
+package dependency
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	nomadapi "github.com/hashicorp/nomad/api"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewSVGetQuery(t *testing.T) {
+
+	cases := []struct {
+		name string
+		i    string
+		exp  *SVGetQuery
+		err  bool
+	}{
+		{
+			"empty",
+			"",
+			&SVGetQuery{},
+			false,
+		},
+
+		{
+			"dc_only",
+			"@dc1",
+			nil,
+			true,
+		},
+		{
+			"path",
+			"path",
+			&SVGetQuery{
+				path: "path",
+			},
+			false,
+		},
+		{
+			"dots",
+			"path.with.dots",
+			&SVGetQuery{
+				path: "path.with.dots",
+			},
+			false,
+		},
+		{
+			"slashes",
+			"path/with/slashes",
+			&SVGetQuery{
+				path: "path/with/slashes",
+			},
+			false,
+		},
+		{
+			"dashes",
+			"path-with-dashes",
+			&SVGetQuery{
+				path: "path-with-dashes",
+			},
+			false,
+		},
+		{
+			"leading_slash",
+			"/leading/slash",
+			&SVGetQuery{
+				path: "leading/slash",
+			},
+			false,
+		},
+		{
+			"trailing_slash",
+			"trailing/slash/",
+			&SVGetQuery{
+				path: "trailing/slash/",
+			},
+			false,
+		},
+		{
+			"underscores",
+			"path_with_underscores",
+			&SVGetQuery{
+				path: "path_with_underscores",
+			},
+			false,
+		},
+		{
+			"special_characters",
+			"config/facet:größe-lf-si",
+			&SVGetQuery{
+				path: "config/facet:größe-lf-si",
+			},
+			false,
+		},
+		{
+			"splat",
+			"config/*/timeouts/",
+			&SVGetQuery{
+				path: "config/*/timeouts/",
+			},
+			false,
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d_%s", i, tc.name), func(t *testing.T) {
+			act, err := NewSVGetQuery(tc.i)
+			if (err != nil) != tc.err {
+				t.Fatal(err)
+			}
+
+			if act != nil {
+				act.stopCh = nil
+			}
+
+			assert.Equal(t, tc.exp, act)
+		})
+	}
+	fmt.Println("done")
+}
+
+func TestSVGetQuery_Fetch(t *testing.T) {
+
+	type svmap map[string]string
+	_ = testNomad.CreateSecureVariable("test-kv-get/path", svmap{"bar": "barp"})
+
+	cases := []struct {
+		name string
+		i    string
+		exp  interface{}
+	}{
+		{
+			"exists",
+			"test-kv-get/path",
+			NewNomadSecureVariable(&nomadapi.SecureVariable{
+				Namespace: "default",
+				Path:      "test-kv-get/path",
+				Items: nomadapi.SecureVariableItems{
+					"bar": "barp",
+				},
+			}).Items,
+		},
+		{
+			"no_exist",
+			"test-kv-get/not/a/real/path/like/ever",
+			nil,
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d_%s", i, tc.name), func(t *testing.T) {
+			d, err := NewSVGetQuery(tc.i)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			act, _, err := d.Fetch(testClients, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if tc.exp != nil {
+				testNomadSVEquivalent(t, tc.exp, act)
+			} else {
+				assert.Nil(t, act)
+			}
+		})
+	}
+
+	t.Run("stops", func(t *testing.T) {
+		d, err := NewSVGetQuery("test-kv-get/path")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		dataCh := make(chan interface{}, 1)
+		errCh := make(chan error, 1)
+		go func() {
+			for {
+				data, _, err := d.Fetch(testClients, nil)
+				if err != nil {
+					errCh <- err
+					return
+				}
+				dataCh <- data
+			}
+		}()
+
+		select {
+		case err := <-errCh:
+			t.Fatal(err)
+		case <-dataCh:
+		}
+
+		d.Stop()
+
+		select {
+		case err := <-errCh:
+			if err != ErrStopped {
+				t.Fatal(err)
+			}
+		case <-time.After(200 * time.Millisecond):
+			t.Errorf("did not stop")
+		}
+	})
+
+	t.Run("fires_changes", func(t *testing.T) {
+		d, err := NewSVGetQuery("test-kv-get/path")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, qm, err := d.Fetch(testClients, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		dataCh := make(chan interface{}, 1)
+		errCh := make(chan error, 1)
+		go func() {
+			for {
+				data, _, err := d.Fetch(testClients, &QueryOptions{WaitIndex: qm.LastIndex})
+				if err != nil {
+					errCh <- err
+					return
+				}
+				dataCh <- data
+				return
+			}
+		}()
+
+		_ = testNomad.CreateSecureVariable("test-kv-get/path", svmap{"bar": "barp", "car": "carp"})
+
+		select {
+		case err := <-errCh:
+			t.Fatal(err)
+		case data := <-dataCh:
+			exp := NewNomadSecureVariable(&nomadapi.SecureVariable{
+				Namespace: "default",
+				Path:      "test-kv-get/path",
+				Items:     nomadapi.SecureVariableItems{"bar": "barp", "car": "carp"},
+			})
+			testNomadSVEquivalent(t, exp.Items, data)
+		}
+	})
+}
+
+func TestSVGetQuery_String(t *testing.T) {
+
+	cases := []struct {
+		name string
+		i    string
+		exp  string
+	}{
+		{
+			"path",
+			"path",
+			"nomad.secure_variables.get(path)",
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d_%s", i, tc.name), func(t *testing.T) {
+			d, err := NewSVGetQuery(tc.i)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, tc.exp, d.String())
+		})
+	}
+}
+
+func testNomadSVEquivalent(t *testing.T, expIf, actIf interface{}) {
+	if expIf == nil && actIf == nil {
+		return
+	}
+	if expIf == nil || actIf == nil {
+		t.Fatalf("Mismatched nil and value.\na: %v\nb:%v", expIf, actIf)
+	}
+	exp, ok := expIf.(NomadSVItems)
+	require.True(t, ok, "exp is not NomadSVItems, got %T", expIf)
+	act, ok := actIf.(NomadSVItems)
+	require.True(t, ok, "act is not NomadSVItems, got %T", actIf)
+
+	expMeta := exp.Metadata()
+	actMeta := act.Metadata()
+	require.NotNil(t, expMeta, "Expected non-nil expMeta.Metadata()")
+	require.NotNil(t, actMeta, "Expected non-nil intMeta.Metadata()")
+	require.Equal(t, expMeta.Namespace, actMeta.Namespace)
+	require.Equal(t, expMeta.Path, actMeta.Path)
+	require.ElementsMatch(t, exp.Tuples(), act.Tuples())
+}
