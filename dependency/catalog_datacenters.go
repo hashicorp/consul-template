bb042bbec3cbfa06b81dd0504f48817efe9d3f1e
scott boone <scott.e.boone@gmail.com>
2023-04-25 14:37:45 -0700

    remove line

--- dependency/catalog_datacenters.go
+++ dependency/catalog_datacenters.go
@@ -88,8 +88,6 @@ func (d *CatalogDatacentersQuery) Fetch(clients *ClientSet, opts *QueryOptions) 
 
 	log.Printf("[TRACE] %s: returned %d results", d, len(result))
 
-	// sort.Strings(result)
-
 	return respWithMetadata(result)
 }
 
