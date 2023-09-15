38facdc72c090cf03c1042629edb09fa651d8bdb
Ville Vesilehto <ville@vesilehto.fi>
2023-09-15 17:13:59 +0300

    chore: remove explicit math/rand seed (#1793)

--- child/child.go
+++ child/child.go
@@ -19,12 +19,6 @@ import (
 	"github.com/hashicorp/consul-template/signals"
 )
 
-func init() {
-	// Seed the default rand Source with current time to produce better random
-	// numbers used with splay
-	rand.Seed(time.Now().UnixNano())
-}
-
 var (
 	// ErrMissingCommand is the error returned when no command is specified
 	// to run.
