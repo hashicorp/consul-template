6cbdad30ecf122701936108979b7e3be892461c2
divyaac <divyaac@berkeley.edu>
2024-05-06 13:07:11 -0700

    Edited test reversed mount path and path

--- dependency/vault_read.go
+++ dependency/vault_read.go
@@ -211,7 +211,7 @@ func shimKVv2Path(rawPath, mountPath, clientNamespace string) string {
 
 		// Trim (mount path - client namespace) from the raw path
 		p := strings.TrimPrefix(rawPath, rawPathNsAndMountPath)
-		
+
 		// Only add /data/ prefix to the path if neither /data/ or /metadata/ are
 		// present.
 		if strings.HasPrefix(p, "data/") || strings.HasPrefix(p, "metadata/") {
