2687db2de50c9f5cc19ad41a291d215a110c93fc
jm96441n <john.maguire@hashicorp.com>
2024-05-23 13:43:15 -0400

    more tests

--- dependency/consul_exported_services_test.go
+++ dependency/consul_exported_services_test.go
@@ -9,44 +9,148 @@ import (
 )
 
 func TestListExportedServicesQuery_Fetch(t *testing.T) {
-	_, _, err := testClients.Consul().ConfigEntries().Set(&capi.ExportedServicesConfigEntry{
-		Name:      "default",
-		Partition: "default",
-		Services: []capi.ExportedService{
-			{
-				Name: "service1",
-				Consumers: []capi.ServiceConsumer{
+	testCases := map[string]struct {
+		partition        string
+		exportedServices *capi.ExportedServicesConfigEntry
+		expected         []ExportedService
+	}{
+		//"no services": {},
+		"default partition - one exported service - partitions set": {
+			partition: "default",
+			exportedServices: &capi.ExportedServicesConfigEntry{
+				Name:      "default",
+				Partition: "default",
+				Services: []capi.ExportedService{
 					{
-						Partition: "default.bar",
+						Name: "service1",
+						Consumers: []capi.ServiceConsumer{
+							{
+								Partition: "foo",
+							},
+						},
+					},
+				},
+			},
+			expected: []ExportedService{
+				{
+					Service: "service1",
+					Consumers: ResolvedConsumers{
+						Peers:          []string{},
+						Partitions:     []string{"foo"},
+						SamenessGroups: []string{},
 					},
 				},
 			},
 		},
-	}, nil)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	q := &ListExportedServicesQuery{
-		stopCh:    make(chan struct{}),
-		partition: "default",
-	}
-
-	actual, _, err := q.Fetch(testClients, nil)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	expected := []ExportedService{
-		{
-			Service: "service1",
-			Consumers: ResolvedConsumers{
-				Peers:          []string{},
-				Partitions:     []string{"default.bar"},
-				SamenessGroups: []string{},
+		"default partition - multiple exported services - partitions set": {
+			partition: "default",
+			exportedServices: &capi.ExportedServicesConfigEntry{
+				Name:      "default",
+				Partition: "default",
+				Services: []capi.ExportedService{
+					{
+						Name: "service1",
+						Consumers: []capi.ServiceConsumer{
+							{
+								Partition: "foo",
+							},
+						},
+					},
+					{
+						Name: "service2",
+						Consumers: []capi.ServiceConsumer{
+							{
+								Partition: "foo",
+							},
+						},
+					},
+				},
+			},
+			expected: []ExportedService{
+				{
+					Service: "service1",
+					Consumers: ResolvedConsumers{
+						Peers:          []string{},
+						Partitions:     []string{"foo"},
+						SamenessGroups: []string{},
+					},
+				},
+				{
+					Service: "service2",
+					Consumers: ResolvedConsumers{
+						Peers:          []string{},
+						Partitions:     []string{"foo"},
+						SamenessGroups: []string{},
+					},
+				},
+			},
+		},
+		"non default partition - multiple exported services - partitions set": {
+			partition: "foo",
+			exportedServices: &capi.ExportedServicesConfigEntry{
+				Name:      "foo",
+				Partition: "foo",
+				Services: []capi.ExportedService{
+					{
+						Name: "service1",
+						Consumers: []capi.ServiceConsumer{
+							{
+								Partition: "default",
+							},
+						},
+					},
+					{
+						Name: "service2",
+						Consumers: []capi.ServiceConsumer{
+							{
+								Partition: "default",
+							},
+						},
+					},
+				},
+			},
+			expected: []ExportedService{
+				{
+					Service: "service1",
+					Consumers: ResolvedConsumers{
+						Peers:          []string{},
+						Partitions:     []string{"default"},
+						SamenessGroups: []string{},
+					},
+				},
+				{
+					Service: "service2",
+					Consumers: ResolvedConsumers{
+						Peers:          []string{},
+						Partitions:     []string{"default"},
+						SamenessGroups: []string{},
+					},
+				},
 			},
 		},
 	}
 
-	require.ElementsMatch(t, expected, actual)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			_, _, err := testClients.Consul().ConfigEntries().Set(tc.exportedServices, &capi.WriteOptions{Partition: tc.partition})
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			q := &ListExportedServicesQuery{
+				stopCh:    make(chan struct{}),
+				partition: tc.partition,
+			}
+
+			actual, _, err := q.Fetch(testClients, nil)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			require.ElementsMatch(t, tc.expected, actual)
+
+			// need to clean up because we use a single shared consul instance
+			_, err = testClients.Consul().ConfigEntries().Delete(capi.ExportedServices, tc.exportedServices.Name, &capi.WriteOptions{Partition: tc.partition})
+		})
+	}
 }
