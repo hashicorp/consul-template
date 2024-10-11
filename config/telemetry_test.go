80287f937750313208602f6929e2714be6d45b90
William Deveaux <william@batch.com>
2024-10-11 11:23:15 +0200

    feat(config/telemetry): adds config merge tests

--- config/telemetry_test.go
+++ config/telemetry_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,4 +30,98 @@ func TestPromConfigParsing(t *testing.T) {
 	config.Finalize()
 	require.Equal(t, 9110, config.Telemetry.PrometheusPort)
 	require.Equal(t, 120*time.Second, config.Telemetry.PrometheusRetentionTime)
+}
+
+func TestTelemetryNilEmptyConfigMerge(t *testing.T) {
+	var nilConfig *TelemetryConfig
+	require.Nil(t, nilConfig.Merge(nil))
+
+	emptyConfig := &TelemetryConfig{}
+	require.Equal(t, emptyConfig, nilConfig.Merge(emptyConfig))
+	require.Equal(t, emptyConfig, emptyConfig.Merge(nil))
+}
+
+func TestTelemetryPartialConfigMerge(t *testing.T) {
+	// Partial configuration merge test
+	partialConfigA := &TelemetryConfig{
+		MetricsPrefix:   "prefix",
+		Disable:         true,
+		AllowedPrefixes: []string{"allowedPrefixA"},
+		StatsdAddr:      "statsA",
+	}
+
+	partialConfigB := &TelemetryConfig{
+		MetricsPrefix:   "new_prefix",
+		Disable:         false,
+		BlockedPrefixes: []string{"prefix"},
+	}
+
+	configC := partialConfigA.Merge(partialConfigB)
+	require.NotEqual(t, configC, partialConfigB)
+
+	require.Equal(t, "new_prefix", configC.MetricsPrefix)
+	require.False(t, configC.Disable)
+	require.Equal(t, []string{"allowedPrefixA"}, configC.AllowedPrefixes)
+	require.Equal(t, []string{"prefix"}, configC.BlockedPrefixes)
+	require.Equal(t, "statsA", configC.StatsdAddr)
+}
+
+func TestTelemetryFullConfigMerge(t *testing.T) {
+	configA := &TelemetryConfig{
+		Disable:                            false,
+		CirconusAPIApp:                     "appA",
+		CirconusAPIToken:                   "tokenA",
+		CirconusAPIURL:                     "apiUrlA",
+		CirconusBrokerID:                   "brokerA",
+		CirconusBrokerSelectTag:            "brokerTagA",
+		CirconusCheckDisplayName:           "A",
+		CirconusCheckForceMetricActivation: false,
+		CirconusCheckID:                    "idA",
+		CirconusCheckInstanceID:            "instanceA",
+		CirconusCheckSearchTag:             "searchTagA",
+		CirconusCheckTags:                  "tagA",
+		CirconusSubmissionInterval:         "1ms",
+		CirconusSubmissionURL:              "urlA",
+		DisableHostname:                    false,
+		DogstatsdAddr:                      "addrA",
+		DogstatsdTags:                      []string{"dsTagA1", "dsTagA2"},
+		FilterDefault:                      false,
+		AllowedPrefixes:                    []string{"allowedPrefixA"},
+		BlockedPrefixes:                    []string{"blockedPrefixA"},
+		MetricsPrefix:                      "prefixA",
+		StatsdAddr:                         "statsA",
+		StatsiteAddr:                       "statsiteA",
+		PrometheusPort:                     8080,
+		PrometheusRetentionTime:            2 * time.Hour,
+	}
+
+	configB := &TelemetryConfig{
+		Disable:                            true,
+		CirconusAPIApp:                     "appB",
+		CirconusAPIToken:                   "tokenB",
+		CirconusAPIURL:                     "apiUrlB",
+		CirconusBrokerID:                   "brokerB",
+		CirconusBrokerSelectTag:            "brokerTagB",
+		CirconusCheckDisplayName:           "B",
+		CirconusCheckForceMetricActivation: true,
+		CirconusCheckID:                    "idB",
+		CirconusCheckInstanceID:            "instanceB",
+		CirconusCheckSearchTag:             "searchTagB",
+		CirconusCheckTags:                  "tagB",
+		CirconusSubmissionInterval:         "1ms",
+		CirconusSubmissionURL:              "urlB",
+		DisableHostname:                    true,
+		DogstatsdAddr:                      "addrB",
+		DogstatsdTags:                      []string{"dsTagB3"},
+		FilterDefault:                      true,
+		AllowedPrefixes:                    []string{"allowedPrefixB"},
+		BlockedPrefixes:                    []string{"blockedPrefixB"},
+		MetricsPrefix:                      "prefixB",
+		StatsdAddr:                         "statsB",
+		StatsiteAddr:                       "statsiteB",
+		PrometheusPort:                     9090,
+		PrometheusRetentionTime:            10 * time.Minute,
+	}
+
+	assert.Equal(t, configB, configA.Merge(configB))
 }
